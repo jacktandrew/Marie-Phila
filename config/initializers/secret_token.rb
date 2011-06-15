f9cb743dbbede5c275adc742ccff4ea893f4fe79
Jack Andrew <jacktandrew@gmail.com>
2011-06-15 09:23:34 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MariePhila::Application.config.secret_token = 'f69bf2e5862a8ff1328a1536deb3e90429a710d64687fd6abb679d8180b4b3ddc9d5f8d2e7dac1cbd7d543f710f7f4714470409522ae4b81816d15379c9a7437'
