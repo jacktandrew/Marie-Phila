dc48003cad9e71a34761c80e2162832f2165d949
Jack Andrew <jacktandrew@gmail.com>
2011-06-20 05:22:54 -0700

    add content and titles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 MariePhila::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
-
+  get "pages/about"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -52,8 +51,8 @@ MariePhila::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
+  root :to => "pages#home"
+   
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
